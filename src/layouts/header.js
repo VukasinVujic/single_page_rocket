e0a9cb91988bfe07df5800529109fe28e3b51fe8
VukasinVujic <vukasinvujic021@gmail.com>
2020-02-24 18:43:48 +0100

    header 2

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import logo from '../images/logo.png';
+import rocket1 from '../images/rocket1.png';
+import rocket2 from '../images/rocket2.png';
+import rocket3 from '../images/rocket3.png';
+import rocket4 from '../images/rocket4.png';
+
+
 
 class Header extends React.Component {
 
@@ -16,13 +22,26 @@ class Header extends React.Component {
                     <li className="singleItemHeader"><a  href="#about">PRICING</a></li>
                     <li className="singleItemHeader"><a  href="#about">CONTACT US</a></li>
                 </ul>
-                    {/* <div className="headerList">
+                    <br/> <br/>
+                    <div className="headerList" >
+                    <span className="rocketPicture"><img src={rocket2} alt=""/></span>
+                    <span className="cloudsPicture"><img src={rocket4} alt=""/></span>
+                    <span className="starsPicture"><img src={rocket3} alt=""/></span>
+
+
                     <h3>Grow your Buisness with</h3>
                     <h3>Our Marketing Solutions</h3>
-                    <p>We help our clients to increase their webiste</p> */}
+                    <p>We help our clients to increase their webiste</p>
+                    <p>trafic, ranking and visibility in search results</p>
+                    <button className="button">try it for free</button>
+                    </div>
+                    <br/><br/><br/><br/><br/>
+                    <br/><br/><br/><br/>
 
-                    {/* </div> */}
+
+                
                 </div>
+                
             </div>
 
         )
