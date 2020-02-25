1a0bb271f24854baa565bca04d7d7737c48ee10a
VukasinVujic <vukasinvujic021@gmail.com>
2020-02-25 20:59:15 +0100

    fixed sticky header

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -8,26 +8,10 @@ class Header extends React.Component {
 
     render(){
         
-        document.addEventListener('DOMContentLoaded', function(){
-            window.addEventListener('scroll', myFunction);
-
-        let header = document.getElementById("myHeader")
-        let sticky = header.offsetTop;
-
-        function myFunction(){
-            if(window.pageXOffset > sticky){ // add "=" for sticky to work
-                header.classList.add("sticky");
-            }else {
-                header.classList.remove("sticky");
-            }
-        }
-
-        })
-
         return(
             <div>
-                <div className="header">
-                <ul className="headerList" id="myHeader">
+            <div  id="myHeader">
+                <ul className="headerList">
                     <li className="singleItemHeader"><a href=""><img src={logo} alt="some picture"/></a></li>
                     <li className="singleItemHeader"><a  href="#home">HOME</a></li>
                     <li className="singleItemHeader"><a  href="#whatWeDo">WHAT WE DO</a></li>
@@ -35,21 +19,33 @@ class Header extends React.Component {
                     <li className="singleItemHeader"><a  href="#about">PRICING</a></li>
                     <li className="singleItemHeader"><a  href="#about">CONTACT US</a></li>
                 </ul>
-                    <br/> <br/>
-                    <div className="headerList" >
-                    <span className="rocketPicture"><img src={rocket2} alt=""/></span>
-                    <span className="cloudsPicture"><img src={rocket4} alt=""/></span>
-                    <span className="starsPicture"><img src={rocket3} alt=""/></span>
+            </div>
 
 
+            <div>
+
+                <div className="header">
+                
+                    <div className="headerContent" >
+                    
+                    <div>
                     <h3>Grow your Buisness with</h3>
                     <h3>Our Marketing Solutions</h3>
                     <p>We help our clients to increase their webiste</p>
                     <p>trafic, ranking and visibility in search results</p>
                     <button className="button">try it for free</button>
                     </div>
+                    </div>
+
+                <div className="wrapper">
+                    <span className="rocketPicture"><img src={rocket2} alt=""/></span>
+                    <span className="cloudsPicture"><img src={rocket4} alt=""/></span>
+                    <span className="starsPicture"><img src={rocket3} alt=""/></span>
+                </div>
+
                     <br/><br/><br/><br/><br/>
                     <br/><br/><br/><br/>
+
                 </div>
                 {/* <div id="whatWeDo">
                 <h1 >what we do </h1>
@@ -57,6 +53,7 @@ class Header extends React.Component {
                 </div>
                  */}
                 
+            </div>
             </div>
 
         )
--- src/mainPart/whatWeDo.js
+++ src/mainPart/whatWeDo.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import blob from '../images/blob1.svg';
-import { library } from '@fortawesome/fontawesome-svg-core';
+// import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStroopwafel,faSearch, faColumns, faShareAlt, faLightbulb, faFolderOpen, faHeart } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter, faFacebookF, faInstagram, faYoutube, faVimeo, faVimeoV } from '@fortawesome/free-brands-svg-icons';
+import { faSearch, faColumns, faShareAlt, faTasks } from '@fortawesome/free-solid-svg-icons';
+// faLightbulb, faFolderOpen, faHeart
 
+// import { faTwitter, faFacebookF, faInstagram, faYoutube, faVimeo, faVimeoV } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faStroopwafel)
+// faStroopwafel
+// library.add(faStroopwafel)
 
 class WhatWeDo extends React.Component{
     render(){
@@ -17,13 +19,17 @@ class WhatWeDo extends React.Component{
                 <p>The full service we are offering is specifically</p>
                 <p>designed to meet your business needs</p>
                 </div>
-                <div className="icons&Text">
-                    {/* <img src={blob} alt=""/> */}
-
+                <div className="blob">
+                    <img src={blob} alt="" className="searchBlob"/><FontAwesomeIcon icon={faSearch} color="orange" className="faDownload" />
+                    <img src={blob} alt="" className="columnsBlob" />
+                    <img src={blob} alt="" className="shareBlob" />
+                    <img src={blob} alt="" className="tasksBlob"/>
                 </div>
-                <FontAwesomeIcon icon={faSearch} color="orange" />
+                {/* <h1>dfsdfsdfds</h1> */}
+                
                 <FontAwesomeIcon icon={faColumns} />
                 <FontAwesomeIcon icon={faShareAlt} />
+                <FontAwesomeIcon icon={faTasks} />
                 
                 <br/>
                 {/* <FontAwesomeIcon icon={faTwitter} />
